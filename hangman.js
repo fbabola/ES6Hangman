c76ac6cd701a87da173d04b462c4ed4009367004
fbabola <39572432+fbabola@users.noreply.github.com>
2018-11-04 17:55:56 -0500

    Add error handling, method to check game completion status

--- hangman.js
+++ hangman.js
@@ -1,4 +1,30 @@
-const NewGame = (word, guessesAllowed) => {
-    this.word = word,
-    this.guessesAllowed = guessesAllowed
+'use strict'
+
+const NewGame = function (word, guessesAllowed) {
+    if ((typeof word) !== 'string' || (typeof guessesAllowed) !== 'number') {
+        throw Error('Give a string for the word to be guessed and a number for the guesses allowed');
+    };
+    this.word = word.toLowerCase().split(''),
+    this.guessesAllowed = guessesAllowed,
+    this.guessedLetters = []
 };
+
+// Gets word puzzle, with correct letter guesses filled in, to be shown to player
+NewGame.prototype.getPuzzle = function () {
+    let puzzle = '';
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter;
+        } else {
+            puzzle += '*';
+        }
+    });
+
+    return puzzle;
+}
+
+let game = new NewGame('yeah', 3);
+console.log(game);
+console.log(game.getPuzzle());
+
