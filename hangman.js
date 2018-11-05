862ee9e6bc1e70267e3e34150f543875a5cff0e5
fbabola <39572432+fbabola@users.noreply.github.com>
2018-11-04 19:54:36 -0500

    Add webpage for display purposes, add guess functionality

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const NewGame = function (word, guessesAllowed) {
-    if ((typeof word) !== 'string' || (typeof guessesAllowed) !== 'number') {
+const Game = function (word, guessesAllowed) {
+    if (typeof word !== 'string' || typeof guessesAllowed !== 'number') {
         throw Error('Give a string for the word to be guessed and a number for the guesses allowed');
     };
     this.word = word.toLowerCase().split(''),
@@ -10,7 +10,7 @@ const NewGame = function (word, guessesAllowed) {
 };
 
 // Gets word puzzle, with correct letter guesses filled in, to be shown to player
-NewGame.prototype.getPuzzle = function () {
+Game.prototype.getPuzzle = function () {
     let puzzle = '';
 
     this.word.forEach((letter) => {
@@ -24,7 +24,35 @@ NewGame.prototype.getPuzzle = function () {
     return puzzle;
 }
 
-let game = new NewGame('yeah', 3);
-console.log(game);
-console.log(game.getPuzzle());
+Game.prototype.guessLetter = function (letter) {
+    if (typeof letter !== 'string' || letter.length !== 1) {
+        throw Error('You must give a single letter');
+    };
+    letter = letter.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(letter);
+    const isWrongGuess = !this.word.includes(letter);
+
+    // Accounting for unique and non-unique guesses
+    if (isUnique) {
+        this.guessedLetters.push(letter);
+    } else if (!isUnique) {
+        throw Error(`You have already guessed ${letter}`);
+    };
+
+    // Hangman mechanic of game
+    if (isUnique && isWrongGuess) {
+        this.guessesAllowed -= 1;
+    };
+};
+
+let game = new Game('yeah', 3);
+
+
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode);
+    game.guessLetter(guess);
+    console.log(game.getPuzzle());
+    console.log(`You have ${game.guessesAllowed} guesses left.`);
+})
 
